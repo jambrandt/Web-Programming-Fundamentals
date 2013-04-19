29b1f9f9acba60a36a2522c1363c556381c03725
Jessica Brandt <SchoolWork@Jessicas-MacBook-Pro.local>
2013-04-18 21:11:26 -0600

    SecondTry

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,19 @@
-alert("Hello World");// JavaScript
-// Jessica Brandt 
+// JavaScript
+// Jessica Brandt April 18th, 2013 Assignment: Expressions
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
+
+
+var restaurantType = ["Fast Food","Family Dining","Bistro","Formal Restaurant"];
+
+var restaurantType[0] = 7; //rought average of price per meal
+var restaurantType[1] = 10;//rought average of price per meal
+var restaurantType[2] = 15;//rought average of price per meal
+var restaurantType[3] = 25;//rought average of price per meal
+
+var howManyPeople = 4; //size of family
+
+var frequency = 3;  //number of times per month 
+
+console.log(restaurantType[1]*howManyPeople*frequency)
+
