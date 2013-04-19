501feb6523391462f0d3db8a6442894e6d182e09
Jessica Brandt <SchoolWork@Jessicas-MacBook-Pro.local>
2013-04-18 21:57:59 -0600

    All errors gone
    Same error for each of the first two. Syntex Errors.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,20 @@
-alert("Hello World");// JavaScript
+// JavaScript
 // Jessica Brandt 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
+
+var storageUnit = ['Small','Medium','Large']
+
+storageUnit[0] = 29; //number of units rented
+storageUnit[1] = 59; //number of units rented
+storageUnit[2] = 27; //number of units rented
+
+var totalOfTwo = storageUnit[0] + storageUnit[1];
+
+var allUnits = 115;
+
+var divide = totalOfTwo / allUnits;
+
+var percent = divide * 100;
+
+console.log(percent);
